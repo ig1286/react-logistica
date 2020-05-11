cb8eb129a2a91a41a97f2dc51660ae7758be288c
Isaias Gandhi <isaias.gandhi@gmail.com>
2020-05-11 12:15:59 -0300

    Mock

---
+++ src/Mock.js
@@ -0,0 +1,28 @@
+ const dados = [ 
+    
+    { cliente:'Walker S.A',
+      descricao:'Tonners',
+      quantidade:'50',
+      entrada:'20/03/2020',
+      saida:'25/03/2020',
+      dimensao:'30x50x70',
+      valor:'8.000,00'
+    },
+    { cliente:'Lucas S.A',
+      descricao:'Parafusos',
+      quantidade:'14',
+      entrada:'02/03/2020',
+      saida:'10/03/2020',
+      dimensao:'10x30x30',
+      valor:'2.000,00'
+    },
+    { cliente:'Buiuvisque S.A',
+      descricao:'Sangue',
+      quantidade:'10',
+      entrada:'05/03/2020',
+      saida:'06/03/2020',
+      dimensao:'20x10x30',
+      valor:'5.000,00'
+    },
+    ]
+export default dados
